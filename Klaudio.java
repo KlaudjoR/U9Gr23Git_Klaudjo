21760e625f327415ee69bd8d1988188a9777865e
klaud <klaudio.rrapo@yahoo.com>
2022-08-10 20:55:35 +0100

    Second commit from Klaudio

--- Klaudio.java
+++ Klaudio.java
@@ -1,5 +1,6 @@
 public class Klaudio {
     public static void main(String[] args) {
         System.out.println("First change from Klaudio");
+        System.out.println("Second change from Klaudio");
     }
 }
